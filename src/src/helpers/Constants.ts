a0087dc129309d5d5c1339e284c65b5cf5704b3e
Gadir Novruzov <108051019+gnovruzov-ld@users.noreply.github.com>
2023-06-12 15:43:42 +0400

    Initial commit
    Initial commit

---
+++ src/src/helpers/Constants.ts
@@ -0,0 +1,9 @@
+export class Constants {
+  public static stsAuthority = 'https://localhost:5555/';
+  public static clientId = 'fusion-web';
+  public static clientSecret = 'ClientSecret1';
+  public static clientRoot = 'http://localhost:4200/';
+  public static clientScope = 'openid profile FusionAPI.read offline_access';
+
+  public static apiRoot = 'https://demo.identityserver.io/api/';
+}
